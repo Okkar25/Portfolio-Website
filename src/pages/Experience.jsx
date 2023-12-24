a7daf489a631b70da7dbc374f60af81759161603
Okkar25 <okkaaung2508@gmail.com>
2023-12-24 18:11:22 +0630

    exp layout changes added

--- src/pages/Experience.jsx
+++ src/pages/Experience.jsx
@@ -1,4 +1,5 @@
 import SchoolIcon from "@mui/icons-material/School";
+import StarIcon from "@mui/icons-material/Star";
 import WorkIcon from "@mui/icons-material/Work";
 import React from "react";
 import {
@@ -22,7 +23,8 @@ const Experience = () => {
           <h3 className="vertical-timeline-element-title">
             B.E.H.S (1) Thingangyun
           </h3>
-          <p>High School Diploma</p>
+          <p className="expTitle">High School Diploma</p>
+          <p className="expLabel"> &#128313; 5 Distinctions </p>
         </VerticalTimelineElement>
 
         {/* YTU */}
@@ -35,7 +37,10 @@ const Experience = () => {
           <h3 className="vertical-timeline-element-title">
             Yangon Technological University
           </h3>
-          <p>Bachelor of Engineering (Electronics)</p>
+          <p className="expTitle">Bachelor of Engineering </p>
+          <p className="expLabel">
+            &#128313; Electronics and Communication Engineering
+          </p>
         </VerticalTimelineElement>
 
         {/* AIESEC */}
@@ -46,7 +51,14 @@ const Experience = () => {
           icon={<WorkIcon />}
         >
           <h3 className="vertical-timeline-element-title">AIESEC in YTU</h3>
-          <p>Incoming Global Volunteering</p>
+          <p className="expTitle">Incoming Global Volunteering</p>
+          <p className="expLabel">
+            &#128313; Hosting global volunteers and providing assistance in
+            their environmental projects.
+          </p>
+          <p className="expLabel">
+            &#128313; Networking and External Public Relations.
+          </p>
         </VerticalTimelineElement>
 
         {/* YSE */}
@@ -59,7 +71,14 @@ const Experience = () => {
           <h3 className="vertical-timeline-element-title">
             Youth Society for Education
           </h3>
-          <p>Content Creator</p>
+          <p className="expTitle">Content Creator</p>
+          <p className="expLabel">
+            &#128313; Manage Facebook page as an admin and publish monthly
+            articles to gain followers.
+          </p>
+          <p className="expLabel">
+            &#128313; Create contents for Events and Awarding Ceremonies.
+          </p>
         </VerticalTimelineElement>
 
         {/* Norman */}
@@ -72,7 +91,21 @@ const Experience = () => {
           <h3 className="vertical-timeline-element-title">
             Norman Myanmar Co.,Ltd
           </h3>
-          <p>Order Processor</p>
+          <p className="expTitle">Order Processor</p>
+          <p className="expLabel">
+            &#128313; Processing overseas orders according to departmental
+            guidelines and policies, analyzing them while ensuring the accuracy
+            of each order assigned.
+          </p>
+          <p className="expLabel">
+            &#128313; Contributing to the growth of the team’s capabilities
+            which include assisting to build more scalable processes, tracking
+            and reporting on order status, making calculations to meet
+            customers’ demands.
+          </p>
+          <p className="expLabel">
+            &#128313; Providing AutoCAD drawings to customer's requirements.
+          </p>
         </VerticalTimelineElement>
 
         {/* Developer */}
@@ -85,8 +118,24 @@ const Experience = () => {
           <h3 className="vertical-timeline-element-title">
             Front-End Developer
           </h3>
-          <p>Open to Work</p>
+          <p className="expTitle">"Open to Work"</p>
+          <p className="expLabel">
+            &#128313; Skilled in HTML, CSS, Bootstrap, TailwindCSS and
+            appropriate understanding of JavaScript / TypeScript and
+            User-friendly interface designs. Committed to producing high-quality
+            work, continuously improving skills and welcoming feedback
+          </p>
+          <p className="expLabel">
+            &#128313; Committed to producing high-quality work in time to meet
+            deadlines, continuously improving skills and welcoming feedbacks.
+          </p>
         </VerticalTimelineElement>
+
+        <VerticalTimelineElement
+          className="vertical-timeline-element--star"
+          iconStyle={{ background: "#7ae582", color: "#fff" }}
+          icon={<StarIcon />}
+        />
       </VerticalTimeline>
     </div>
   );
